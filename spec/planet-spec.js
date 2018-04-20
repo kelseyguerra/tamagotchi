29650772c3bb726278fbb3199cd2bcc9292d102c
Adrianacmy <adrianamullaney@gmail.com>
2018-04-20 14:56:30 -0700

    add test for earthAgeToSecond()

--- spec/planet-spec.js
+++ spec/planet-spec.js
@@ -1,14 +1,20 @@
 import {Planet} from "../src/planet";
+import {LivingBeing} from "../src/livingBeing";
 
 describe("Planet", function(){
   let resuablePlanet;
+  let livingBeing = new LivingBeing(11, 89);
   beforeEach(function(){
-    resuablePlanet = new Planet(11, 89);
+    resuablePlanet = new Planet('earth', livingBeing);
   });
 
-  it("should has value of 89 on property of avgLifespan when resuablePlanet is created", function(){
-    expect(resuablePlanet.avgLifespan).toEqual(89);
+  it("should has value of 89 on property of avgLifespan when resuablePlanet is created", () => {
+    expect(resuablePlanet.name).toEqual("earth");
+    expect(livingBeing.avgLifespan).toEqual(89);
   });
 
+  it("should return earth age in seconds", () => {
+    expect(resuablePlanet.earthAgeToSeconds(10)).toEqual(10*365*24*60*60);
+  });
 
 });
