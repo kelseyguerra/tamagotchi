273436228c6c980386b5c9c981aa83462187da8e
Adrianacmy <adrianamullaney@gmail.com>
2018-04-20 17:48:29 -0700

    add test for nextBirthdayDate()

--- spec/galacticAgeCaculator-spec.js
+++ spec/galacticAgeCaculator-spec.js
@@ -1,15 +1,33 @@
 import {GalacticAgeCaculator} from "../src/galacticAgeCaculator"
+import {Planet} from "../src/planet";
+import {LivingBeing} from "../src/livingBeing"
 
 describe("GalacticAgeCaculator", function(){
   let resuableCalcs;
+  let planet;
+  let livingBeing
   beforeEach(function(){
-    resuableCalcs = new GalacticAgeCaculator("venus", 10);
+    livingBeing = new LivingBeing("March 21, 2012", 89);
+    planet = new Planet('Mars', 1.88, livingBeing);
+    resuableCalcs = new GalacticAgeCaculator(planet, livingBeing);
+
   });
 
-  it("should has value of 'venus' and 10 after an instance created", function(){
-    expect(resuableCalcs.earthAge).toEqual(10);
-    expect(resuableCalcs.planet).toEqual("venus");
+  it("should has value of 'earth' and 10 after an instance created", function(){
+    expect(resuableCalcs.planet).toBeObject();
+    expect(resuableCalcs.livingBeing).toBeObject();
   });
+
+
+  it("should return current planet age", () => {
+    expect(resuableCalcs.planetAge()).toBeGreaterThan(1.88*5.9);
+  });
+
+  it("should return ", () => {
+    expect(resuableCalcs.nextBirthdayDate()).toBeDate();
+
+  })
 
 
 });
+
